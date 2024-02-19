02718cb97b67f64156fadecfeb4668410b239f14
hugoresende <hugo.resende@iservices.pt>
2024-02-19 22:24:27 +0000

    first

---
+++ README.md
@@ -0,0 +1,2 @@
+### Champions Manger 2024
+- this is a classic football manager
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body id="app" class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100">
+</body>
+</html>
