e2420d1a1259daaaca6b5c4ff2cd5b263a64ae3e
hugoresende <hugo.resende@iservices.pt>
2024-03-24 20:07:09 +0000

    feature : load game working

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -7,16 +7,17 @@
 }
 </style>
 <aside
-    {{-- class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" --}}
+
     class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark"
     id="sidenav-main">
     <div class="sidenav-header">
-        <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
-            aria-hidden="true" id="iconSidenav"></i>
+       
         <a class="navbar-brand m-0 d-flex text-wrap align-items-center" href="#">
 
-            <span class="ms-2 font-weight-bold text-white">{{ date('d/m/y - H.i') }}</span>
+            <span class="ms-2 font-weight-bold text-white">{{ date(' d/m/y - H : i') }}</span>
+           
         </a>
+ 
     </div>
     <hr class="horizontal light mt-0 mb-0">
     <div class="  w-auto  max-height-vh-100" id="sidenav-collapse-main">
@@ -149,6 +150,11 @@
         </ul> --}}
     </div>
     <div class="sidenav-footer position-absolute w-100 bottom-0 ">
+        <div class="mx-3">
+            <a class="btn  w-100" href=""
+             style="background-color: green;
+             color: white">NEXT DAY</a>
+        </div>
         <div class="mx-3">
             <a class="btn  w-100" href=""
              style="background-color: {{(App\Models\Team::getColors(auth()->user()->about)[0])}};
