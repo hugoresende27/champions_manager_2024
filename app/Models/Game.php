3edce93749b6fe85b89a8871799a4eefefc895e2
hugoresende <hugo.resende@iservices.pt>
2024-03-25 19:51:19 +0000

    feature : game calendar

---
+++ app/Models/Game.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'team_id',
+        'season_win',
+        'season_draw',
+        'season_lost',
+        'season_points',
+        'season_games_played',
+        'total_win',
+        'total_draw',
+        'total_lost',
+        'total_points',
+        'total_games_played',
+        'total_time_played',
+        'current_date',
+    ];
+}
---
+++ database/migrations/2024_03_25_192558_create_games_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('team_id')->foreign('about')->references('id')->on('teams')->onDelete('set null');
+
+            $table->integer('season_win')->default(0);
+            $table->integer('season_draw')->default(0);
+            $table->integer('season_lost')->default(0);
+            $table->integer('season_points')->default(0);
+            $table->integer('season_games_played')->default(0);
+
+            $table->integer('total_win')->default(0);
+            $table->integer('total_draw')->default(0);
+            $table->integer('total_lost')->default(0);
+            $table->integer('total_points')->default(0);
+            $table->integer('total_games_played')->default(0);
+            $table->integer('total_time_played')->default(0);
+            
+            $table->date('current_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
